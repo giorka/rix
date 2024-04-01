fe2116f0839f77d3fc8651a469e22a999d0e76cc
giorka <mokenosov@ro.ru>
2024-04-01 22:51:35 +0300

    + Automatic Login To Your Profile Upon Registration

--- server/v1/views.py
+++ server/v1/views.py
@@ -1,8 +1,12 @@
 from typing import Type
 
+from django.contrib.auth import login
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 
 from . import serializers
+from .models import User
 
 
 class RegisterAPIView(generics.CreateAPIView):
@@ -11,3 +15,12 @@ class RegisterAPIView(generics.CreateAPIView):
 
 class VerifyAPIView(generics.CreateAPIView):
     serializer_class: Type[serializers.UserRegisterFormSerializer] = serializers.UserVerificationSerializer
+
+    def post(self, request, *args, **kwargs) -> Response:
+        serializer_class: Serializer = self.serializer_class(data=request.data)
+        serializer_class.is_valid(raise_exception=True)
+
+        user: User = serializer_class.save()['user']
+        login(request=request, user=user)
+
+        return Response(serializer_class.validated_data)
