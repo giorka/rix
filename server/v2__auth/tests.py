b622a16923e77dc089a4b8f50975cb8ba9604098
giorka <mokenosov@ro.ru>
2024-06-11 20:13:31 +0300

    / Register Tests, - Revert Tests

--- server/v2__auth/tests.py
+++ server/v2__auth/tests.py
@@ -1,79 +1,125 @@
 from __future__ import annotations
 
-from django.test import TestCase
-from rest_framework.reverse import reverse
-from v2.utils import get_object_or_404
+import logging
+from typing import Any
+
+from django.urls import reverse
+from rest_framework.response import Response
+from rest_framework.test import APITestCase
 
 from . import models
-from . import utils
-
-register_tests_map = [
-    (
-        201,
-        {
-            'data': {'username': 'test', 'email': 'test@test.com'},
-            'create_data': {'password': '12345678a$'},
-        },
-    ),
-    (
-        400,
-        {
-            'data': {'username': 'test', 'email': 'test'},
-            'create_data': {'password': '12345678a$'},
-        },
-    ),
-    (
-        400,
-        {
-            'data': {'username': 'test', 'email': 'test@test.com'},
-            'create_data': {'password': '123'},
-        },
-    ),
-]
-register_good_status_codes = (201,)
-
-revert_good_status_codes = (201,)
 
 
-class AuthTestCase(TestCase):
-    def test_register(self) -> None:
-        for status_code, details in register_tests_map:
-            response = self.client.post(reverse('register'), data=details['data'] | details['create_data'])
+def logging_decorator(target: callable) -> callable:
+    def wrapper(*args, **kwargs) -> Any:
+        response = target(*args, **kwargs)
 
-            self.assertEqual(status_code, response.status_code, msg='Status Code Does Not Match Expected')
+        logging.debug(str(response.content, 'UTF-8'))
 
-            is_exists = models.User.objects.filter(**details['data']).exists()
+        return response
 
-            self.assertEqual(is_exists, status_code in register_good_status_codes)
+    return wrapper
 
-            models.User.objects.all().delete()
 
-    def test_revert(self) -> None:
+class ClientMixin:
+    @logging_decorator
+    def post(self: PathBasedAPITestCase, data: dict) -> Response:
+        return self.client.post(self.path, data=data)
+
+
+class PathBasedAPITestCase(APITestCase):
+    path = 'example/url/'
+
+
+class UserCreateAPIViewTestCase(PathBasedAPITestCase, ClientMixin):
+    path = reverse('register')
+
+    def test_all_ok(self) -> None:
         data = {
-            'username': 'test',
-            'email': 'test@test.com',
+            'username': 'test',  # good username
+            'email': 'good_email@gmail.com',  # good email
+            'password': '123456789good_password$',  # good password
         }
 
-        user = models.User(**data)
-        user.set_password(raw_password='12345678a$')
-        user.is_verified = True
-        user.save()
+        response = self.post(data)
 
-        document = {'email': data['email']}
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(models.User.objects.filter(username=data['username'], email=data['email']).exists())
 
-        response = self.client.post(reverse('revert'), data=document)
+    def test_taken_username(self) -> None:
+        """
+        Test to verify that an API does not work with taken usernames.
+        """
 
-        self.assertTrue(response.status_code in revert_good_status_codes, msg='Revert Request Failed')
+        models.User(username='taken_username').save()
 
-        code = utils.email.revert_queue.find({'email': data['email']})['code']
-        new_password = '987654321$'
-        document |= {'code': code, 'new_password': new_password}
+        data = {
+            'username': 'taken_username',  # bad username
+            'email': 'good_email@gmail.com',  # good email
+            'password': '123456789good_password$',  # good password
+        }
 
-        response = self.client.post(reverse('revert-complete'), data=document)
+        response = self.post(data)
 
-        self.assertTrue(response.status_code in revert_good_status_codes, msg='Revert Confirmation Request Failed')
+        self.assertEqual(response.status_code, 400)
 
-        user = get_object_or_404(models.User, email=data['email'], is_verified=True)
-        is_password_set = user.check_password(new_password)
+    def test_invalid_email(self) -> None:
+        """
+        Test to verify that an API does not work with invalid emails.
+        """
 
-        self.assertTrue(is_password_set, msg='Error Setting Password')
+        data = {
+            'username': 'test',  # good username
+            'email': 'invalid_email',  # bad email
+            'password': '123456789good_password$',  # good password
+        }
+
+        response = self.post(data)
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_simple_password(self) -> None:
+        """
+        Test to verify that an API does not work with simple passwords.
+        """
+
+        data = {
+            'username': 'test',  # good username
+            'email': 'good_email@gmail.com',  # good email
+            'password': '123',  # bad password
+        }
+
+        response = self.post(data)
+
+        self.assertEqual(response.status_code, 400)
+
+
+#     def test_revert(self) -> None:
+#         data = {
+#             'username': 'test',
+#             'email': 'test@test.com',
+#         }
+#
+#         user = models.User(**data)
+#         user.set_password(raw_password='12345678a$')
+#         user.is_verified = True
+#         user.save()
+#
+#         document = {'email': data['email']}
+#
+#         response = self.client.post(reverse('revert'), data=document)
+#
+#         self.assertTrue(response.status_code in revert_good_status_codes, msg='Revert Request Failed')
+#
+#         code = utils.email.revert_queue.find({'email': data['email']})['code']
+#         new_password = '987654321$'
+#         document |= {'code': code, 'new_password': new_password}
+#
+#         response = self.client.post(reverse('revert-complete'), data=document)
+#
+#         self.assertTrue(response.status_code in revert_good_status_codes, msg='Revert Confirmation Request Failed')
+#
+#         user = get_object_or_404(models.User, email=data['email'], is_verified=True)
+#         is_password_set = user.check_password(new_password)
+#
+#         self.assertTrue(is_password_set, msg='Error Setting Password')
