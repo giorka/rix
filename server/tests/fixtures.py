51291c1e5ce419d9b47b4099be7de349b11e10fb
giorka <mokenosov@ro.ru>
2024-06-16 17:42:57 +0300

    + MongoDB Flusher, + Revert Complete Tests

--- server/tests/fixtures.py
+++ server/tests/fixtures.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 import pytest
 
 from v2.utils.tests import LoggerAPIClient as APIClient
+from v2__auth import utils
 
 
 @pytest.fixture
-def client():
+def client() -> APIClient:
     return APIClient()
--- server/tests/v2/test_auth.py
+++ server/tests/v2/test_auth.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import json
+
 import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
@@ -43,7 +45,12 @@ def test_register(client: APIClient, username: str, email: str, password: str, s
         (constants.faker.user_name(), 'nonexistent@gmail.com', 404),
     ),
 )
-def test_revert(client: APIClient, username: str, email: str, status_code: int) -> None:
+def test_revert(
+    client: APIClient,
+    username: str,
+    email: str,
+    status_code: int,
+) -> None:
     is_going_correct = status_code == 201
 
     models.User(username=username, email=email, is_verified=is_going_correct).save()
@@ -52,3 +59,53 @@ def test_revert(client: APIClient, username: str, email: str, status_code: int) 
 
     assert response.status_code == status_code
     assert bool(utils.email.revert_queue.find(email)) is is_going_correct
+
+    utils.email.revert_queue.flush()
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    ('username', 'email', 'password', 'status_code'),
+    (
+        (constants.faker.user_name(), constants.faker.email(), constants.faker.password(length=8), 201),
+        (constants.faker.user_name(), constants.faker.email(), constants.faker.password(length=8), 404),
+        (constants.faker.user_name(), constants.faker.email(), constants.faker.password(length=8), 400),
+    ),
+)
+def test_revert_complete(
+    client: APIClient,
+    username: str,
+    email: str,
+    password: str,
+    status_code: int,
+) -> None:
+    if status_code != 404:
+        models.User(username=username, email=email, is_verified=True).save()
+
+    utils.email.revert_queue.add(email)
+
+    code: str = utils.email.revert_queue.find(email)['code']
+
+    if status_code == 400:
+        invalid_code: str = utils.email.EmailService.generate_code()
+
+        while invalid_code == code:
+            invalid_code = utils.email.EmailService.generate_code()
+
+        code = invalid_code
+
+    response = client.post(
+        reverse('revert-complete'),
+        data={
+            'email': email,
+            'code': code,
+            'new_password': password,
+        },
+    )
+
+    assert response.status_code == status_code
+
+    if status_code == 201:
+        assert json.loads(response.content).get('auth_token') is not None
+
+    utils.email.revert_queue.flush()
