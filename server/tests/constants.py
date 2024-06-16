c7b71200bc46801885d82fed4648bec8eaec20f8
giorka <mokenosov@ro.ru>
2024-06-16 17:55:09 +0300

    / Minimize Register Tests

--- server/tests/constants.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from faker import Faker
-
-faker = Faker()
--- server/tests/fixtures.py
+++ server/tests/fixtures.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import pytest
 
 from v2.utils.tests import LoggerAPIClient as APIClient
-from v2__auth import utils
 
 
 @pytest.fixture
